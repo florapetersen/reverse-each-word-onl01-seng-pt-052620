c4e2f3e3c55af320f25d0266cbc9543383d8d3f4
Flora Petersen <floramarina@comcast.net>
2020-06-05 05:12:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  array = string.split
-  return_array = []
-  array.each do |word|
-    return_array << word.reverse
-  end
-  return_array.join(" ")
-end
+#def reverse_each_word(string)
+  #array = string.split
+  #return_array = []
+  #array.each do |word|
+  #  return_array << word.reverse
+  #end
+  #return_array.join(" ")
+#end
 
 def reverse_each_word_with_collect(string)
   array = string.split
